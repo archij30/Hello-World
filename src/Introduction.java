232fd626089ebd6487135f156c3168f798f58ccb
archi <archij30@gmail.com>
2021-01-11 02:04:52 +0530

    Hello world in java

---
+++ src/Introduction.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author archi.jain
+ *
+ */
+public class Introduction {
+
+	/**
+	 * print hello world 
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
